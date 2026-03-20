e1f6720b9ca98dd646c83dcfb889d744faf878f3
simondev413 <devemail413@gmail.com>
2026-03-20 21:29:56 +0100

    adiciona arquivo main.py com configuração da API e rota inicial; adiciona arquivo requests.http para testes de requisições; cria diretório src/utils com __init__.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from src.routes.sheduled_payments_routes import router as sheduled_payments_router
+
+
+app = FastAPI(title="Sheduled Payments API", description="API for managing scheduled payments", version="1.0.0")
+app.include_router(sheduled_payments_router)
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Scheduled Payments API!",}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run('main:app', host="localhost", port=8000,reload=True)
+    
