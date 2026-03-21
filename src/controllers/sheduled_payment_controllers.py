a5363abc4d035fee09f8cbca83e350013282176a
simondev413 <devemail413@gmail.com>
2026-03-21 18:23:23 +0100

    adiciona tratamento de erros

--- src/controllers/sheduled_payment_controllers.py
+++ src/controllers/sheduled_payment_controllers.py
@@ -1,33 +1,37 @@
-from src.services.sheduled_payments_service import SheduledPaymentService
-from src.models.sheduled_payment import SheduledPayment
-from fastapi import Request,Response
+from fastapi import HTTPException, Response, status
 
+from src.models.sheduled_payment_model import SheduledPaymentCreate
+from src.services.sheduled_payments_service import SheduledPaymentService
 
 
 class SheduledPaymentController:
     def __init__(self, service: SheduledPaymentService) -> None:
         self.service = service
 
-    async def create_sheduled_payment(self, request: Request, response: Response):
-        request_data = await request.json()
-        sheduled_payment = SheduledPayment(**request_data)
-        validated_data = SheduledPayment.model_validate(sheduled_payment)
-        created_payment = await self.service.create_sheduled_payment(validated_data)
-        response.status_code = 201
-        return created_payment.model_dump()
+    async def create_sheduled_payment(self, sheduled_payment: SheduledPaymentCreate, response: Response):
+        created_payment = await self.service.create_sheduled_payment(sheduled_payment)
+        response.status_code = status.HTTP_201_CREATED
+        return created_payment
 
     async def list_sheduled_payments(self):
         return await self.service.list_sheduled_payments()
 
     async def get_sheduled_payment(self, sheduled_payment_id: str):
-        return await self.service.get_sheduled_payment(sheduled_payment_id)
+        sheduled_payment = await self.service.get_sheduled_payment(sheduled_payment_id)
+        if sheduled_payment is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Scheduled payment not found")
+        return sheduled_payment
 
-    async def update_sheduled_payment(self, sheduled_payment_id: str, request: Request):
-        sheduled_payment_data = await request.json()
-        sheduled_payment = SheduledPayment.model_validate(**sheduled_payment_data)
-        return await self.service.update_sheduled_payment(sheduled_payment_id, sheduled_payment)
+    async def update_sheduled_payment(self, sheduled_payment_id: str, sheduled_payment: SheduledPaymentCreate):
+        updated_payment = await self.service.update_sheduled_payment(sheduled_payment_id, sheduled_payment)
+        if updated_payment is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Scheduled payment not found")
+        return updated_payment
 
     async def delete_sheduled_payment(self, sheduled_payment_id: str):
-        return await self.service.delete_sheduled_payment(sheduled_payment_id)
+        deleted = await self.service.delete_sheduled_payment(sheduled_payment_id)
+        if not deleted:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Scheduled payment not found")
+        return {"deleted": True}
     
    
