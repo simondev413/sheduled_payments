705fb3e31110e1815b0b9f1cd7d985d78f39c735
simondev413 <devemail413@gmail.com>
2026-03-21 18:26:47 +0100

    adicioando lifespan para gerenciamento de base de dados

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from fastapi import FastAPI
+from src.database.database import lifespan
 from src.routes.sheduled_payments_routes import router as sheduled_payments_router
 
 
-app = FastAPI(title="Sheduled Payments API", description="API for managing scheduled payments", version="1.0.0")
+
+app = FastAPI(
+    title="Sheduled Payments API",
+    description="API for managing scheduled payments",
+    version="1.0.0",
+    lifespan=lifespan,
+)
 app.include_router(sheduled_payments_router)
+
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to the Scheduled Payments API!",}
+    return {"message": "Welcome to the Scheduled Payments API!"}
+
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run('main:app', host="localhost", port=8000,reload=True)
-    
+
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
